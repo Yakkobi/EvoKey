72f341425bcb707473b40377ce7b74b42e913e29
Yakkobi <289114684+Yakkobi@users.noreply.github.com>
2026-08-12 17:27:42 -0400

    Add settings menu
    Added a menu for settings, and added the
    listener for the settings button and the
    Main Menu button within the settings menu.

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function makeVisible(id) {
   document.querySelectorAll(".visible").forEach((visible) => {
     visible.classList.toggle("hidden", visible.id !== id);
   });
-  document.querySelector("header").classList.toggle("hidden", id !== "mainMenu");
+  document
+    .querySelector("header")
+    .classList.toggle("hidden", id !== "mainMenu");
 }
 
 document
   .getElementById("macrosButton")
   .addEventListener("click", () => makeVisible("macrosMenu"));
+
+document
+  .getElementById("settingsButton")
+  .addEventListener("click", () => makeVisible("settingsMenu"));
 
 document.querySelectorAll(".backButton").forEach((button) => {
   button.addEventListener("click", () => makeVisible(button.dataset.target));
